2c05c7dd2c91e0b49284f96306f3de68a91e9945
kefa802 <kefa802@gmail.com>
2026-05-07 13:27:56 +0800

    AppDatabase.java の更新

--- app/src/main/java/com/example/gpslog/AppDatabase.java
+++ app/src/main/java/com/example/gpslog/AppDatabase.java
@@ -1,7 +1,9 @@
 package com.example.gpslog;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
-@Database(entities = {LocationEntity.class, LocationLogEntity.class}, version = 2, exportSchema = false)
+
+// versionを 3 に変更 ✅
+@Database(entities = {LocationEntity.class, LocationLogEntity.class}, version = 3, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract LocationDao locationDao();
 }
