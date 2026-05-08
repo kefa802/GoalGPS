6faa77a6795529726da72b5cc9e1731c0b73a70b
kefa802 <kefa802@gmail.com>
2026-05-08 12:04:30 +0800

    DailyAccumulator.java の更新

--- app/src/main/java/com/example/gpslog/DailyAccumulator.java
+++ app/src/main/java/com/example/gpslog/DailyAccumulator.java
@@ -1,11 +1,12 @@
 package com.example.gpslog;
 
+import androidx.annotation.NonNull;
 import androidx.room.Entity;
-import androidx.room.PrimaryKey;
 
 @Entity(tableName = "daily_totals", primaryKeys = {"locationId", "date"})
 public class DailyAccumulator {
     public int locationId;
+    @NonNull
     public String date; // "yyyy-MM-dd"
     public long totalInMs;
     public long totalOutMs;
